2b4806f92c62d96312ff9ccc1b25e396ba6ad84d
Asqka <asiatbahmudova081@gmail.com>
2025-09-29 11:36:42 +0300

    fix

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    publhjic static voghghd main(String[] args) {
         String message = "Hello";
         messadge = "World"; // первое значение не используется!
         System.sout.println(message);
