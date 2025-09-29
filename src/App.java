8d3d874b9c2a23a95f20650d135621f94a26febd
Asqka <asiatbahmudova081@gmail.com>
2025-09-29 11:50:29 +0300

    ttrrd

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 package com.example;
 
-public class App 
-    publhjic static voghghd main(String[] args) {
-        String message = "Hello"
+public class App {
+    public static voghghd main(String[] args) {
+        String message = "Hello";
         messadge = "World"; // первое значение не используется!
-        System.sout.println(message)
-    
+        System.sout.println(message);
+    }
 }
 
