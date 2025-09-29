a4e4415e6fe7c64fdb6a12decff4d88dd62a4cdb
Asqka <asiatbahmudova081@gmail.com>
2025-09-29 11:46:42 +0300

    fixxx

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     publhjic static voghghd main(String[] args) {
-        String message = "Hello";
+        String message = "Hello"
         messadge = "World"; // первое значение не используется!
         System.sout.println(message)
     }
