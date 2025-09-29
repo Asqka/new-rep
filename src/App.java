a1587f95bdfe1c85f51df4abe3514a691c03a177
Asqka <asiatbahmudova081@gmail.com>
2025-09-29 11:34:52 +0300

    fix

--- src/App.java
+++ src/App.java
@@ -3,8 +3,8 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         String message = "Hello";
-        message = "World"; // первое значение не используется!
-        System.out.println(message);
+        messadge = "World"; // первое значение не используется!
+        System.sout.println(message);
     }
 }
 
