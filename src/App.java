13f170fb529b1c2f362698ca3275c97db630a2e3
Asqka <asiatbahmudova081@gmail.com>
2025-09-29 11:18:21 +0300

    pusg

---
+++ src/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        String message = "Hello";
+        message = "World"; // первое значение не используется!
+        System.out.println(message);
+    }
+}
+
